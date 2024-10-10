1e99eb2379905db19f0a076bcb3e9baea7f2a9fb
Daniel Romero <danieljromero305@gmail.com>
2024-10-10 12:29:09 -0400

    autoformat

--- src/imgStitch.java
+++ src/imgStitch.java
@@ -4,13 +4,13 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class imgStitch {
-    public static void main(String[] args){
-        try{
-            BufferedImage combined = new BufferedImage(12*3000, 10*3000, BufferedImage.TYPE_INT_RGB); //crashes through intellij, need to use terminal with at least 4gb xmx
-            for(int i = 0;i<=9;i++) {
+    public static void main(String[] args) {
+        try {
+            BufferedImage combined = new BufferedImage(12 * 3000, 10 * 3000, BufferedImage.TYPE_INT_RGB); //crashes through intellij, need to use terminal with at least 4gb xmx
+            for (int i = 0; i <= 9; i++) {
                 for (int abc = 0; abc < 12; abc++) { //if(i==9 && abc == 11){} else {
                     String imgName = Integer.toString(i) + toChar(abc);
-                    System.out.println("src: "+imgName);
+                    System.out.println("src: " + imgName);
 
                     BufferedImage source = ImageIO.read(new File("img\\" + imgName + ".png"));
 
@@ -21,20 +21,22 @@ public class imgStitch {
                     combined  = resize(combined, width, height);*/ //This is useful when constantly expanding, but uses too much memory
 
                     Graphics g = combined.getGraphics();
-                    g.drawImage(source, abc*3000,i*3000,null);
+                    g.drawImage(source, abc * 3000, i * 3000, null);
                     g.dispose();
-                //}
+                    //}
                 }
             }
             //combined = resize(combined,12*3000, 9*3000);
             ImageIO.write(combined, "PNG", new File("img\\", "combined.png"));
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static char toChar(int a){
-        return (char)(a + 65);
+
+    public static char toChar(int a) {
+        return (char) (a + 65);
     }
+
     /*public static BufferedImage resize(BufferedImage img, int newW, int newH) {
         Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
         BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
